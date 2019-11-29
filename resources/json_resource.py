ec58dad60fa1a989f31fa719ebd896634370edb4
Leonardo Silva <psilva.leo@gmail.com>
2019-11-28 23:14:42 -0300

    Getting json parameters

--- resources/json_resource.py
+++ resources/json_resource.py
@@ -1,3 +1,7 @@
+from typing import Tuple
+
+from flask import request
+
 from resources.base_resource import BaseResource
 
 
@@ -12,6 +16,18 @@ class JsonResource(BaseResource):
 
     def post(self):
         request_text = self._get_text()
-        formatted_json, error = self._parser.parse_data(data=request_text)
+        indent, sort_keys = self._get_parameters()
+
+        formatted_json, error = self._parser.parse_data(data=request_text, indent=indent, sort_keys=sort_keys)
 
         return self._render_template(formatted_json=formatted_json, error=error)
+
+    @staticmethod
+    def _get_parameters() -> Tuple[int, bool]:
+        request_data = request.form.to_dict(flat=False)
+        minify = request_data['minify'] if 'minify' in request_data.keys() else None
+        indent = None if minify else int(request_data['indent'][0])
+
+        sort_keys = True if 'sort_keys' in request_data.keys() else False
+
+        return indent, sort_keys

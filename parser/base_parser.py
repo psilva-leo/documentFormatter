e2b83ac6ba07ddd2659d38656f5f89fc9d527a90
andreMarcosPerez <andre.marcos.perez@gmail.com>
2019-11-28 22:23:08 -0300

    removed leftover code

--- parser/base_parser.py
+++ parser/base_parser.py
@@ -1,13 +1,5 @@
 class BaseParser(object):
 
-    def __init__(self):
-
-        self.var = 1
-        self._setup()
-
-    def _setup(self):
-        pass
-
     def parse_data(self, data: str) -> str:
         raise NotImplementedError('Child class must implement parse_data method')
 
